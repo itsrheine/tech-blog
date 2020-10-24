249da57222d7aec7eecf5849c6e78cd2d153ecfe
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-23 22:15:23 -0700

    routes working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require("path");
 const exphbs = require("express-handlebars");
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 
