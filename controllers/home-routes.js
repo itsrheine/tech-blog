01c94ea8e58c6da13baaf81474f25aa065ba5841
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-24 14:46:59 -0700

    ask bcs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,10 +70,8 @@ router.get('/post/:id', (req, res) => {
                 return;
             }
 
-            // serialize the data
             const post = dbPostData.get({ plain: true });
 
-            // pass data to template
             res.render('single-post', {
                 post,
                 loggedIn: req.session.loggedIn
