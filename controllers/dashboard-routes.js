3eb81030be40753c731cda214a849a1d9d137009
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-24 20:51:07 -0700

    ask bcs

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -30,7 +30,7 @@ router.get('/', withAuth, (req, res) => {
     })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('dashboard-main', { layout: "dashboard", posts, loggedIn: true });
+        res.render('dash-main', { layout: "dashboard", posts, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
