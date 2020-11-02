285a00decf083100363c949f97f6b4c41f1ef49d
Marrione Nguyen <mtiamsic@gmail.com>
2020-11-01 20:40:31 -0800

    heroku

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,7 +40,7 @@ router.get('/', withAuth, (req, res) => {
 });
 
 router.get('/new', withAuth, (req, res) => {
-    res.render('new-post', { layout: "main" });
+    res.render('new-post', { layout: "secondary" });
 })
 
 router.get('/post/:id', withAuth, (req, res) => {
